6a3cf989fc7f9c2b1f616b584cbae037a474f4b4
IkanPakUs <komanggede86@gmail.com>
2023-01-17 16:09:56 +0800

    first setup

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="container d-flex flex-column align-items-center">
+    <nav>
+      <router-link to="/dashboard">Home</router-link> |
+      <router-link to="/dashboard/about">About</router-link>
+    </nav>
+    <router-view/>
+  </div>
 </template>
 
 <style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="home">
+  <div class="home d-flex flex-column align-items-center">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
